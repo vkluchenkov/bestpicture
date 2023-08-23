5ad78cf91c49d668cdd3da6ad5285ba70eec4466
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-08-23 18:06:21 +0200

    fix: yml env error

--- wooApi/wooApiREST.ts
+++ wooApi/wooApiREST.ts
@@ -1,7 +1,5 @@
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
 
-console.log(process.env.WC_CONSUMER_KEY);
-
 export const api = new WooCommerceRestApi({
   url: process.env.NEXT_PUBLIC_WORDPRESS_URL!,
   consumerKey: process.env.WC_CONSUMER_KEY!,
